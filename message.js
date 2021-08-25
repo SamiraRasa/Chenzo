a1f5759d6d008a62ffe69ef5593cdaeb7a41f383
Yasaman K <yasaman@m.lo.agency>
2021-08-26 01:10:25 +0430

    feat: go-down

---
+++ message.js
@@ -0,0 +1,2 @@
+let messages = ["You are great!", "You can accomplish anything!", "Success is in your future!"];
+module.exports.messages =["You are great!", "You can accomplish anything!", "Success is in your future!"];
