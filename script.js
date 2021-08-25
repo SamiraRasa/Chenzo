57f9b3a002ff649212adb414ce39018ae04d10ae
Yasaman K <yasaman@m.lo.agency>
2021-08-26 02:50:29 +0430

    fix bug

--- script.js
+++ script.js
@@ -1,22 +1,27 @@
 const hamburger = document.querySelector(".hamburger");
 const navMenu = document.querySelector(".nav-menu");
 
+const body1 = document.querySelector("body");
+
 hamburger.addEventListener("click", mobileMenu);
 
 function mobileMenu() {
     hamburger.classList.toggle("active");
     navMenu.classList.toggle("active");
-    document.body.style.position = "fixed";
+
+    body1.classList.toggle("fix-poz")
     
 }
 
-const navLink = document.querySelectorAll(".nav-item");
+// const navLink = document.querySelectorAll(".nav-item");
 
-navLink.forEach(n => n.addEventListener("click", closeMenu));
+// navLink.forEach(n => n.addEventListener("click", closeMenu));
 
-function closeMenu() {
-    hamburger.classList.remove("active");
-    navMenu.classList.remove("active");
-    document.body.style.position = " "; 
+// function closeMenu() {
+//     hamburger.classList.remove("active");
+//     navMenu.classList.remove("active");
+//     // document.body.style.position = ""; 
+//     body1.classList.remove("fix-poz");
     
-}
+    
+// }
