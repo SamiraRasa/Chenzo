7a8819da625e87d25a2182158941a790d88293e0
Yasaman K <yasaman@m.lo.agency>
2021-08-26 14:34:01 +0430

    feat: add-server

--- message.js
+++
@@ -1,2 +0,0 @@
-let messages = ["You are great!", "You can accomplish anything!", "Success is in your future!"];
-module.exports.messages =["You are great!", "You can accomplish anything!", "Success is in your future!"];
--- script.js
+++ script.js
@@ -12,16 +12,3 @@ function mobileMenu() {
     body1.classList.toggle("fix-poz")
     
 }
-
-// const navLink = document.querySelectorAll(".nav-item");
-
-// navLink.forEach(n => n.addEventListener("click", closeMenu));
-
-// function closeMenu() {
-//     hamburger.classList.remove("active");
-//     navMenu.classList.remove("active");
-//     // document.body.style.position = ""; 
-//     body1.classList.remove("fix-poz");
-    
-    
-// }
